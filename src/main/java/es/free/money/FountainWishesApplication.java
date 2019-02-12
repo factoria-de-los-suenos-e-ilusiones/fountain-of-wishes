b74ebb441f9da26c17f05c539b95d31a01371eb1
Lykansito <robertodelabanda@gmail.com>
2019-02-12 16:09:29 +0100

    Master Branch
    - Initial commit

---
+++ src/main/java/es/free/money/FountainWishesApplication.java
@@ -0,0 +1,14 @@
+package es.free.money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FountainWishesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FountainWishesApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/es/free/money/FountainWishesApplicationTests.java
@@ -0,0 +1,17 @@
+package es.free.money;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FountainWishesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
